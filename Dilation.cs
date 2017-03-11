8dac81b95803b13bae6f4bf70cb00480e8733b40
fredriclam <fredriclam@live.com>
2017-03-11 16:41:49 -0500

    Working 'relativity' with ~limitations~

--- Dilation.cs
+++ Dilation.cs
@@ -7,22 +7,42 @@ public class Dilation : MonoBehaviour {
     public GameObject TrackedObject;
     public static float lightSpeed;
     private Vector3 originalScale;
-    private Rigidbody rb;
+    private Rigidbody trackedRigidBody;
+    private Rigidbody currentRigidBody;
+
+    private Vector3 gamma;
+    private Vector3 lastGamma;
+
 	// Use this for initialization
 	void Start () {
         // Tracked object's rigid body
-        rb = TrackedObject.GetComponent<Rigidbody>();
+        trackedRigidBody = TrackedObject.GetComponent<Rigidbody>();
+        // Track (this)'s rigid body
+        currentRigidBody = this.GetComponent<Rigidbody>();
         // This object's scale
         originalScale = this.transform.localScale;
-	}
+        // Initialize
+        lastGamma = new Vector3(1, 1, 1);
+
+    }
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-        float lightSpeed = SugarController.lightSpeed;
-        Vector3 velocity = -rb.velocity;
-        Vector3 gammaVector = new Vector3(originalScale[0] * Mathf.Sqrt(1 - (velocity[0] / lightSpeed) * (velocity[0] / lightSpeed)),
-            originalScale[1] * Mathf.Sqrt(1 - (velocity[1] / lightSpeed) * (velocity[1] / lightSpeed)),
-            originalScale[2] * Mathf.Sqrt(1 - (velocity[2] / lightSpeed) * (velocity[2] / lightSpeed)));
-        this.transform.localScale = gammaVector;
+        // Grab the tracked reference frame
+        Rigidbody playerReferenceFrame = TrackedObject.GetComponent<Rigidbody>();
+        // Calculate gamma factor
+        Vector3 gamma = SugarController.GammaVector(currentRigidBody.velocity, playerReferenceFrame);
+
+        // Boost the object scale
+        this.transform.localScale = SugarController.LorentzBoostScale(originalScale, gamma);
+
+        // Grab frame position
+        Vector3 framePosition = TrackedObject.transform.position;
+        // Boost
+        this.transform.position = SugarController.InverseBoostedPosition(framePosition, this.transform.position - framePosition, lastGamma);
+        this.transform.position = SugarController.MapToBoostedPosition(framePosition, this.transform.position - framePosition, gamma);
+        
+        // Save relative velocity 
+        lastGamma = gamma;
     }
 }
--- SugarController.cs
+++ SugarController.cs
@@ -9,8 +9,41 @@ public class SugarController : MonoBehaviour {
     public static float lightSpeed = 10;
     private float maxSpeed = 0.9f; 
 
-	// Use this for initialization
-	void Start () {
+    public static Vector3 GammaVector(Vector3 objectVelocity, Rigidbody playerReferenceFrame) {
+        // Grab velocity 
+        Vector3 frameVelocity = playerReferenceFrame.velocity;
+        Vector3 relativeVelocity = frameVelocity - objectVelocity;
+        // Gamma vector
+        return new Vector3(
+            1.0f / Mathf.Sqrt(1 - (relativeVelocity[0] / lightSpeed) * (relativeVelocity[0] / lightSpeed)),
+            1.0f / Mathf.Sqrt(1 - (relativeVelocity[1] / lightSpeed) * (relativeVelocity[1] / lightSpeed)),
+            1.0f / Mathf.Sqrt(1 - (relativeVelocity[2] / lightSpeed) * (relativeVelocity[2] / lightSpeed)));
+    }
+
+    // Affine transformation
+    public static Vector3 MapToBoostedPosition(Vector3 framePosition, Vector3 relativePosition, Vector3 gammaVector)
+    {
+        return framePosition + new Vector3(relativePosition[0] / gammaVector[0],
+            relativePosition[1] / gammaVector[1],
+            relativePosition[2] / gammaVector[2]);
+    }
+
+    public static Vector3 InverseBoostedPosition(Vector3 framePosition, Vector3 relativePosition, Vector3 gammaVector)
+    {
+        return framePosition + new Vector3(relativePosition[0] * gammaVector[0],
+            relativePosition[1] * gammaVector[1],
+            relativePosition[2] * gammaVector[2]);
+    }
+
+    public static Vector3 LorentzBoostScale(Vector3 objectScale, Vector3 gammaVector)
+    {
+        return new Vector3(objectScale[0] / gammaVector[0],
+            objectScale[1] / gammaVector[1],
+            objectScale[2] / gammaVector[2]);
+    }
+
+    // Use this for initialization
+    void Start () {
         rb = this.GetComponent<Rigidbody>();
 	}
 	
